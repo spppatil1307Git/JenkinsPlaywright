e804d5ffa777bd77c2ceb457f0912f9573e665ec
shubham <shubhamppatil1307@gmail.com>
2026-06-07 16:47:42 +0530

    Alure report removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    
   ],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
